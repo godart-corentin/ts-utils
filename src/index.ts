1a263db1bce1ff62caa53dd49719c5facafc4172
Corentin Godart <godartcn@gmail.com>
2023-06-16 22:01:14 +0200

    feat: updated the index.ts barrel

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
+export { chunk } from "./chunk";
+export { first } from "./first";
 export { groupBy } from "./groupBy";
+export { isEqual } from "./isEqual";
+export { last } from "./last";
+export { uniq } from "./uniq";

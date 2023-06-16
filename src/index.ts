4e02b74e756e5ec0b141f51d4f8faf8045b28312
Corentin <godartcn@gmail.com>
2023-06-16 22:12:10 +0200

    chore: reorganize functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-export { chunk } from "./chunk";
-export { first } from "./first";
-export { groupBy } from "./groupBy";
-export { isEqual } from "./isEqual";
-export { last } from "./last";
-export { uniq } from "./uniq";
+export * from "./functions";

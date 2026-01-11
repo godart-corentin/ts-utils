7afb43d72c94ded83801adc5e60d47db2b0f4ed5
Corentin Godart <godartcn@gmail.com>
2026-01-11 19:33:31 +0100

    chore: remove validators

--- src/validation/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./validators";
-export * from "./error";
-export * from "./withSafeParse";
--- src/validation/validators/string/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./string";
-export * from "./email";
-export * from "./url";
-export * from "./uuid";
-export * from "./ip";
